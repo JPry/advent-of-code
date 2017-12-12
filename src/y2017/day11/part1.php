5bf0b83b1738f0de6c158f24f7249b7136de05c3
Jeremy Pry <jeremy.pry@gmail.com>
2017-12-12 08:46:23 -0500

    Some progress on day 11

--- src/y2017/day11/part1.php
+++ src/y2017/day11/part1.php
@@ -33,7 +33,18 @@ namespace JPry\AdventOfCode\y2017\day11;
 
 function reduceOpposites(array $directions)
 {
-    $opposites = [
+    $directions = array_merge(
+        [
+            'n'  => 0,
+            'ne' => 0,
+            'se' => 0,
+            's'  => 0,
+            'sw' => 0,
+            'nw' => 0,
+        ],
+        $directions
+    );
+    $opposites  = [
         'n'  => 's',
         'ne' => 'sw',
         'nw' => 'se',
@@ -48,11 +59,70 @@ function reduceOpposites(array $directions)
     return $directions;
 }
 
-function reduceNeighbors(array $directions) {
+function reduceSides(array $directions)
+{
+    $order  = ['n', 'ne', 'se', 's', 'sw', 'nw'];
+    $length = count($order);
+    $next   = function($index, $jump) use ($length) {
+        $index += $jump;
 
+        return $index >= $length ? $index - $length : $index;
+    };
+
+    foreach ($order as $index => $side) {
+        if (0 === $directions[$side]) {
+            continue;
+        }
+
+        $compare1 = $next($index, 2);
+        $compare2 = $next($compare1, 2);
+
+
+    }
+
+    foreach ($order as $side) {
+        // Skip if there are no steps in the current direction.
+        if (!isset($directions[$side]) || !$directions[$side]) {
+            continue;
+        }
+
+        switch ($side) {
+            case 'sw':
+            case 'se':
+                if ($directions['se'] >= $directions['sw']) {
+                    $directions['s']  += $directions['sw'];
+                    $directions['sw'] = 0;
+                    $directions['se'] -= $directions['sw'];
+                } else {
+                    $directions['s']  += $directions['se'];
+                    $directions['se'] = 0;
+                    $directions['sw'] -= $directions['se'];
+                }
+
+                break;
+
+            case 'nw':
+            case 'ne':
+
+
+                break;
+        }
+    }
+
+    return $directions;
 }
+
+$test1 = 'ne,ne,ne';
+$test2 = 'ne,ne,sw,sw';
+$test3 = 'ne,ne,s,s';
+$test4 = 'se,sw,se,sw,sw';
 
 $input   = explode(',', trim(file_get_contents(__DIR__ . '/input.txt')));
 $counted = array_count_values($input);
-print_r(reduceOpposites($counted));
+$reduced = reduceSides(reduceOpposites($counted));
 
+foreach (range(1, 4) as $i) {
+    echo "steps test{$i}: " . array_sum(
+            reduceSides(reduceOpposites(array_count_values(explode(',', ${"test{$i}"}))))
+        ) . PHP_EOL;
+}
