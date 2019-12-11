ee6b11c59677e91f18d3ef31a4c3ba15ee01e16b
Jeremy Pry <jeremy.pry@gmail.com>
2019-12-11 16:14:40 -0500

    Add template files

---
+++ src/y2019/template/Solver.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace JPry\AdventOfCode\y2019\template;
+
+use JPry\AdventOfCode\DayPuzzle;
+
+class Solver extends DayPuzzle
+{
+	public function testData()
+	{
+	}
+
+	protected function part1()
+	{
+	}
+
+
+	protected function part2()
+	{
+	}
+
+	protected function getNamespace()
+	{
+		return __NAMESPACE__;
+	}
+}
---
+++ src/y2019/template/index.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace JPry\AdventOfCode\y2019\template;
+
+require_once dirname(__FILE__, 4) . '/vendor/autoload.php';
+
+$solver = new Solver();
+$solver->run();
