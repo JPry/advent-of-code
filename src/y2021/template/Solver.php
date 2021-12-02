1a6a16cbff3a73c6415abe45705df241b3db41e3
Jeremy Pry <jeremy.pry@gmail.com>
2021-12-02 16:01:17 -0500

    Update template Solver class

--- src/y2021/template/Solver.php
+++ src/y2021/template/Solver.php
@@ -7,17 +7,29 @@ use JPry\AdventOfCode\DayPuzzle;
 
 class Solver extends DayPuzzle
 {
-	public function testData()
+	public function runTests()
 	{
+		$handle = $this->getHandleForFile('test');
+		$this->part1Logic($handle);
+		$this->part2Logic($handle);
 	}
 
 	protected function part1()
 	{
 	}
 
-
 	protected function part2()
 	{
+	}
+
+	protected function part1Logic($input)
+	{
+
+	}
+
+	protected function part2Logic($input)
+	{
+
 	}
 
 	protected function getNamespace(): string
