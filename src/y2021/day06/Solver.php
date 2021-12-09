0ccc499520832ea7250a86adbb4f418f8ef6c4b4
Jeremy Pry <jeremy.pry@gmail.com>
2021-12-09 09:58:59 -0500

    Add part 2 solution

--- src/y2021/day06/Solver.php
+++ src/y2021/day06/Solver.php
@@ -9,20 +9,28 @@ class Solver extends DayPuzzle
 {
 	public function runTests()
 	{
-		$testData = array_map('intval', explode(',', trim(file_get_contents($this->getFilePath('test')))));
-		$this->part1Logic($testData);
-		$this->part2Logic($testData);
+		$testData = $this->convertInputToFish(file_get_contents($this->getFilePath('test')));
+		$this->part1Logic($testData, 18);
+		$this->part2Logic($testData, 256);
 	}
 
 	protected function part1()
 	{
+		$this->part2Logic(
+			$this->convertInputToFish(file_get_contents($this->getFilePath('input'))),
+			80
+		);
 	}
 
 	protected function part2()
 	{
+		$this->part2Logic(
+			$this->convertInputToFish(file_get_contents($this->getFilePath('input'))),
+			256
+		);
 	}
 
-	protected function part1Logic(array $fishes)
+	protected function part1Logic(array $fishes, int $days)
 	{
 		$dayCount = 0;
 		do {
@@ -40,15 +48,41 @@ class Solver extends DayPuzzle
 			$fishes = array_merge($fishes, array_fill(0, $newFish, 8));
 
 			$dayCount++;
-//			printf("After %d %s: %d\n", $dayCount, $dayCount > 1 ? 'days' : 'day', count($fishes));
-		} while ($dayCount < 80);
+		} while ($dayCount < $days);
 
 		printf("There are %d fish!\n", count($fishes));
 	}
 
-	protected function part2Logic($input)
+	protected function part2Logic(array $fishes, int $days)
 	{
+		$fishForDays = array_replace(
+			array_fill(0, 9, 0),
+			array_count_values($fishes)
+		);
 
+		$dayCount = 0;
+		do {
+			$shiftedFish = array_fill(0, 9, 0);
+			foreach($fishForDays as $index => $count) {
+				if (0 === $index) {
+					$shiftedFish[8] = $count;
+					$shiftedFish[6] = $count;
+					continue;
+				}
+
+				$shiftedFish[$index - 1] += $count;
+			}
+
+			$fishForDays = $shiftedFish;
+
+			$dayCount++;
+		} while ($dayCount < $days);
+		printf("There are %d fish after %d days\n", array_sum($fishForDays), $dayCount);
+	}
+
+	protected function convertInputToFish(string $input): array
+	{
+		return array_map('intval', explode(',', trim($input)));
 	}
 
 	protected function getNamespace(): string
