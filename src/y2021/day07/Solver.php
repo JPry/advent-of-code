39e9e4849360043c5a521e91478797419c3eec8e
Jeremy Pry <jeremy.pry@gmail.com>
2021-12-10 17:32:19 -0500

    Start code for day 7 solution

--- src/y2021/day07/Solver.php
+++ src/y2021/day07/Solver.php
@@ -9,10 +9,9 @@ class Solver extends DayPuzzle
 {
 	public function runTests()
 	{
-		$handle = $this->getHandleForFile('test');
-		$this->part1Logic($handle);
-		rewind($handle);
-		$this->part2Logic($handle);
+		$testData = $this->stringToNumbers($this->getFileContents('test'));
+		$this->part1Logic($testData);
+		$this->part2Logic($testData);
 	}
 
 	protected function part1()
@@ -23,14 +22,39 @@ class Solver extends DayPuzzle
 	{
 	}
 
-	protected function part1Logic($input)
+	protected function part1Logic(array $crabbies)
 	{
+		$min = min($crabbies);
+		$max = max($crabbies);
+
+		// Find the most common number
+		$counts = array_count_values($crabbies);
+		rsort($counts);
+
+		// Let's try some numbers.
+		$trialMin = $counts[0] - 10 < $min ? $min : $counts[0] - 10;
+		$trialMax = $counts[0] + 10 > $max ? $max : $counts[0] + 10;
+		$trials = range($trialMin, $trialMax);
+
+
 
 	}
 
 	protected function part2Logic($input)
 	{
 
+	}
+
+	protected function calculateFuel(array $crabbies, int $number)
+	{
+		$fuels = array_map(
+			function ($value) use ($number) {
+				return abs($value - $number);
+			},
+			$crabbies
+		);
+
+		return array_sum($fuels);
 	}
 
 	protected function getNamespace(): string
