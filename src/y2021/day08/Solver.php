7df00af3e790b3aa1a57d9dba7e23d09346dcaad
Jeremy Pry <jeremy.pry@gmail.com>
2021-12-13 00:09:01 -0500

    Add day 8 part 1 solution

--- src/y2021/day08/Solver.php
+++ src/y2021/day08/Solver.php
@@ -3,6 +3,7 @@
 
 namespace JPry\AdventOfCode\y2021\day08;
 
+use Exception;
 use JPry\AdventOfCode\DayPuzzle;
 
 class Solver extends DayPuzzle
@@ -17,6 +18,7 @@ class Solver extends DayPuzzle
 
 	protected function part1()
 	{
+		$this->part1Logic($this->getHandleForFile('input'));
 	}
 
 	protected function part2()
@@ -25,7 +27,33 @@ class Solver extends DayPuzzle
 
 	protected function part1Logic($input)
 	{
+		$digitCount = 0;
+		while ($line = fgets($input)) {
+			$line = trim($line);
+			if (empty($line)) {
+				continue;
+			}
 
+			[$patterns, $output] = explode(' | ', $line);
+
+			$pieces = explode(' ', $output);
+			if (4 !== count($pieces)) {
+				throw new Exception("Whoops, wrong count.");
+			}
+
+			foreach ($pieces as $piece) {
+				switch (strlen($piece)) {
+					case 2:
+					case 3:
+					case 4:
+					case 7:
+						$digitCount++;
+						break;
+				}
+			}
+		}
+
+		printf("Found %d digits.\n", $digitCount);
 	}
 
 	protected function part2Logic($input)
