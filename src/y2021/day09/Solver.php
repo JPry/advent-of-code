9ceb2f094d7d4f2f1375e03268ca8448f8f595b1
Jeremy Pry <jeremy.pry@gmail.com>
2021-12-14 18:16:06 -0500

    Start adding code for part 2 solution

--- src/y2021/day09/Solver.php
+++ src/y2021/day09/Solver.php
@@ -101,6 +101,67 @@ class Solver extends DayPuzzle
 		);
 	}
 
+	protected function findLowPoints(array $map): array
+	{
+		$points = [];
+		$lastRowindex = count($map) - 1;
+		array_walk(
+			$map,
+			function ($row, $rowIndex) use ($map, &$points, $lastRowindex) {
+				$lastColumnIndex = count($row) - 1;
+				foreach ($row as $columnIndex => $value) {
+					// 9 can never be a low spot.
+					if (9 === $value) {
+						continue;
+					}
+
+					// A 0 must be a low spot... I think?
+					if (0 === $value) {
+						$points[] = [$rowIndex, $columnIndex];
+						continue;
+					}
+
+					// Check all available directions.
+					$comparison = [];
+
+					// To the north.
+					if ($rowIndex > 0) {
+						$comparison['north'] = $map[$rowIndex - 1][$columnIndex] > $value;
+					}
+
+					// To the east.
+					if ($columnIndex < $lastColumnIndex) {
+						$comparison['east'] = $map[$rowIndex][$columnIndex + 1] > $value;
+					}
+
+					// To the south.
+					if ($rowIndex < $lastRowindex) {
+						$comparison['south'] = $map[$rowIndex + 1][$columnIndex] > $value;
+					}
+
+					// To the west.
+					if ($columnIndex > 0) {
+						$comparison['west'] = $map[$rowIndex][$columnIndex - 1] > $value;
+					}
+
+					$isLowest = array_reduce(
+						$comparison,
+						function (bool $carry, bool $value) {
+							return $carry && $value;
+						},
+						true
+					);
+
+					if ($isLowest) {
+						$points[] = [$rowIndex, $columnIndex];
+					}
+				}
+			}
+		);
+
+		return $points;
+	}
+
 	protected function getNamespace(): string
 	{
 		return __NAMESPACE__;
