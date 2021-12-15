be230fe9ffc2e4d427a6d420bcca782be509cc5c
Jeremy Pry <jeremy.pry@gmail.com>
2021-12-15 00:29:04 -0500

    Create Point class

---
+++ src/y2021/day09/Point.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace JPry\AdventOfCode\y2021\day09;
+
+use LogicException;
+
+/**
+ * Class Point
+ *
+ * @property int row
+ * @property int column
+ */
+class Point
+{
+	protected int $row;
+	protected int $column;
+
+	public function __construct(int $row, int $column)
+	{
+		$this->row = $row;
+		$this->column = $column;
+	}
+
+	public static function fromString(string $point): self
+	{
+		[$row, $column] = array_map('intval', explode(',', $point));
+
+		return new self($row, $column);
+	}
+
+	public function __isset($name)
+	{
+		return $name === 'row' || $name === 'column';
+	}
+
+	public function __get($name)
+	{
+		switch ($name) {
+			case 'row':
+				return $this->row;
+			case 'column':
+				return $this->column;
+
+			default:
+				throw new LogicException(sprintf('No such property: %s', $name));
+		}
+	}
+
+	public function __toString(): string
+	{
+		return sprintf('%d,%d', $this->row, $this->column);
+	}
+}
