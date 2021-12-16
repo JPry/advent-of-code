556077aef381cb3b86157b8cc8a2acc2a170712c
Jeremy Pry <jeremy.pry@gmail.com>
2021-12-16 01:45:51 -0500

    Partial solution for day 11

--- src/y2021/day11/Solver.php
+++ src/y2021/day11/Solver.php
@@ -9,10 +9,9 @@ class Solver extends DayPuzzle
 {
 	public function runTests()
 	{
-		$handle = $this->getHandleForFile('test');
-		$this->part1Logic($handle);
-		rewind($handle);
-		$this->part2Logic($handle);
+		$lines = file($this->getFilePath('test'), FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
+		$this->part1Logic($lines);
+		$this->part2Logic($lines);
 	}
 
 	protected function part1()
@@ -23,13 +22,68 @@ class Solver extends DayPuzzle
 	{
 	}
 
-	protected function part1Logic($input)
+	protected function part1Logic(array $lines)
 	{
+		$flashes = 0;
+		$steps = 0;
 
+		$octopi = array_map(
+			function ($line) {
+				return array_map('intval', str_split($line));
+			},
+			$lines
+		);
+
+		do {
+			// Increment each number.
+			array_walk_recursive(
+				$octopi,
+				function (&$value) {
+					$value++;
+				}
+			);
+
+			// Do the flashes.
+			$flashedThisStep = [];
+			do {
+				$didFlashes = false;
+				foreach ($octopi as $rowIndex => $row) {
+					foreach ($row as $columnIndex => $value) {
+						// Each octopus flashes once per step.
+						$key = sprintf('%d,%d', $rowIndex, $columnIndex);
+						if (array_key_exists($key, $flashedThisStep)) {
+							continue;
+						}
+
+						if ($value > 9) {
+							$didFlashes = true;
+							$flashedThisStep[$key] = true;
+							$this->energizeAround($octopi, $rowIndex, $columnIndex);
+						}
+					}
+				}
+			} while ($didFlashes);
+
+			$steps++;
+		} while ($steps < 100);
+
+		printf("There were %d flashes", $flashes);
 	}
 
 	protected function part2Logic($input)
 	{
+
+	}
+
+	protected function energizeAround(array &$octopi, int $row, int $column)
+	{
+		$toEnergize = array_merge(
+
+		);
+		$canDoPriorRow = $row > 0;
+		$canDoNextRow = $row < (count($octopi) - 1);
+		$canDoPriorColumn = $column > 0;
+		$canDoNextColumn = $column < (count($octopi[$row]) - 1);
 
 	}
 
