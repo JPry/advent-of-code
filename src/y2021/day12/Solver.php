e09b76f0cc3883b6ff0e938ab6c8697a0e54bb7f
Jeremy Pry <jeremy.pry@gmail.com>
2021-12-17 12:25:30 -0500

    Add placeholder for day 12

---
+++ src/y2021/day12/Solver.php
@@ -0,0 +1,39 @@
+<?php
+
+
+namespace JPry\AdventOfCode\y2021\day12;
+
+use JPry\AdventOfCode\DayPuzzle;
+
+class Solver extends DayPuzzle
+{
+	public function runTests()
+	{
+		$data = $this->getFileAsArray('test');
+		$this->part1Logic($data);
+		$this->part2Logic($data);
+	}
+
+	protected function part1()
+	{
+	}
+
+	protected function part2()
+	{
+	}
+
+	protected function part1Logic($input)
+	{
+
+	}
+
+	protected function part2Logic($input)
+	{
+
+	}
+
+	protected function getNamespace(): string
+	{
+		return __NAMESPACE__;
+	}
+}
