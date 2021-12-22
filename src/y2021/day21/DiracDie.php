39654c857c7b368ed800f3587acd606f250b9b5c
Jeremy Pry <jeremy.pry@gmail.com>
2021-12-22 17:57:04 -0500

    Create DircaDie class

---
+++ src/y2021/day21/DiracDie.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace JPry\AdventOfCode\y2021\day21;
+
+/**
+ * Class DiracDie
+ *
+ * @since %VERSION%
+ */
+class DiracDie implements RollableDie
+{
+	public function getRolls(int $howMany = 3): array
+	{
+		// TODO: Implement getRolls() method.
+	}
+
+	public function getRoll(): int
+	{
+		// TODO: Implement getRoll() method.
+	}
+
+	public function getRollCount(): int
+	{
+		// TODO: Implement getRollCount() method.
+	}
+
+}
