7f6eeb042fac11a98a1b080b05edbdcddceb56ab
Jeremy Pry <jeremy.pry@gmail.com>
2021-12-21 22:41:03 -0500

    Start working on day 12 solution

--- src/y2021/day12/Solver.php
+++ src/y2021/day12/Solver.php
@@ -9,7 +9,7 @@ class Solver extends DayPuzzle
 {
 	public function runTests()
 	{
-		$data = $this->getFileAsArray('test');
+		$data = $this->getFileAsArray('test1');
 		$this->part1Logic($data);
 		$this->part2Logic($data);
 	}
@@ -22,14 +22,57 @@ class Solver extends DayPuzzle
 	{
 	}
 
-	protected function part1Logic($input)
+	protected function part1Logic(array $input)
 	{
-
+		$map = $this->parseLines($input);
+		$paths = $this->countPaths($map, 'start');
 	}
 
 	protected function part2Logic($input)
 	{
 
+	}
+
+	protected function parseLines(array $input): array
+	{
+		$return = [];
+		foreach ($input as $line) {
+			[$point, $connection] = explode('-', $line);
+			if (!array_key_exists($point, $return)) {
+				$return[$point] = [];
+			}
+
+			if (!array_key_exists($connection, $return)) {
+				$return[$connection] = [];
+			}
+
+			$return[$point][] = $connection;
+			$return[$connection][] = $point;
+		}
+
+		foreach ($return as &$values) {
+			$values = array_unique($values);
+		}
+
+		return $return;
+	}
+
+	protected function countPaths(array &$map, string $point)
+	{
+		static $visited = [];
+		$validPaths = 0;
+
+		if (array_key_exists($point, $visited)) {
+			return $validPaths;
+		}
+
+		if (strtolower($point) === $point) {
+			$visited[$point] = true;
+		}
+
+		foreach ($map[$point] as $caves) {
+
+		}
 	}
 
 	protected function getNamespace(): string
