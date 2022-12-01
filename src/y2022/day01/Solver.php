3157d172bd1db0efc01e39d4b7ffcd8889007d78
Jeremy Pry <jeremy.pry@gmail.com>
2022-12-01 13:52:16 -0500

    Add solutions to day 1

--- src/y2022/day01/Solver.php
+++ src/y2022/day01/Solver.php
@@ -9,27 +9,71 @@ class Solver extends DayPuzzle
 {
 	public function runTests()
 	{
-		$data = $this->getFileAsArray('test');
-		$this->part1Logic($data);
+		$data = $this->getHandleForFile('test');
+//		$this->part1Logic($data);
 		$this->part2Logic($data);
 	}
 
 	protected function part1()
 	{
+		$this->part1Logic($this->getHandleForFile('input'));
 	}
 
 	protected function part2()
 	{
+		$this->part2Logic($this->getHandleForFile('input'));
 	}
 
-	protected function part1Logic($input)
+	/**
+	 * @param resource $input
+	 * @return array
+	 */
+	protected function part1Logic($input): array
 	{
+		$elves = [0 => []];
+		$elf = 0;
+		while (false !== ($line = fgets($input))) {
+			$line = trim($line);
+			if ('' === $line) {
+				$elf++;
+				$elves[$elf] ??= [];
+				continue;
+			}
 
+			$elves[$elf][] = (int) $line;
+		}
+
+		fclose($input);
+
+		$currentMax = 0;
+		$maxElf = 0;
+		foreach ($elves as $number => $foodItems) {
+			$calories = array_sum($foodItems);
+			$elves[$number]['sum'] = $calories;
+			if ($calories > $currentMax) {
+				$currentMax = $calories;
+				$maxElf = $number;
+			}
+		}
+
+		printf("Elf %d had the most calories: %d\n", $maxElf, $currentMax);
+
+		return $elves;
 	}
 
+	/**
+	 * @param resource $input
+	 * @return void
+	 */
 	protected function part2Logic($input)
 	{
+		$elfData = $this->part1Logic($input);
+		$sums = array_column($elfData, 'sum');
+		rsort($sums);
 
+		$top3 = array_slice($sums, 0, 3);
+
+		printf("The top 3 elves had %d calories in total.\n", array_sum($top3));
 	}
 
 	protected function getNamespace(): string
