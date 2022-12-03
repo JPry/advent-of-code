d3b5cb24211f6f9c2550fd83a10cbcf94e773767
Jeremy Pry <jeremy.pry@gmail.com>
2022-12-03 00:29:18 -0500

    Add day 3 input and solution

--- src/y2022/day03/Solver.php
+++ src/y2022/day03/Solver.php
@@ -16,24 +16,67 @@ class Solver extends DayPuzzle
 
 	protected function part1()
 	{
+		$this->part1Logic($this->getFileAsArray('input'));
 	}
 
 	protected function part2()
 	{
+		$this->part2Logic($this->getFileAsArray('input'));
 	}
 
 	protected function part1Logic($input)
 	{
+		$map = $this->getScoreMap();
+		$count = 0;
+		foreach ($input as $line) {
+			[$first, $second] = str_split($line, strlen($line)/2);
+			$firstPieces = array_fill_keys(str_split($first), 1);
+			$secondPieces = array_fill_keys(str_split($second), 1);
 
+			$leftover = array_intersect_key($firstPieces, $secondPieces);
+			$count += $map[array_flip($leftover)[1]];
+		}
+
+		printf("The count was: %d\n", $count);
 	}
 
 	protected function part2Logic($input)
 	{
+		$map = $this->getScoreMap();
+		$count = 0;
 
+		do {
+			// get 3 items.
+			$pieces[] = array_shift($input);
+			$pieces[] = array_shift($input);
+			$pieces[] = array_shift($input);
+
+			foreach ($pieces as &$piece) {
+				$piece = array_fill_keys(str_split($piece), 1);
+			}
+
+			$common = array_intersect_key(...$pieces);
+			$count += $map[array_flip($common)[1]];
+
+			$pieces = [];
+		} while (count($input) >= 3);
+
+		printf("The count was: %d\n", $count);
 	}
 
 	protected function getNamespace(): string
 	{
 		return __NAMESPACE__;
+	}
+
+	/**
+	 * @return array|false
+	 */
+	protected function getScoreMap()
+	{
+		return array_combine(
+			array_merge(range('a', 'z'), range('A', 'Z')),
+			range(1, 52)
+		);
 	}
 }
