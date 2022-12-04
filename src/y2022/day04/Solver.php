79fe6306e0f2ae136b717d61afefec15686846f4
Jeremy Pry <jeremy.pry@gmail.com>
2022-12-04 00:15:02 -0500

    Add input and solution for day 4

--- src/y2022/day04/Solver.php
+++ src/y2022/day04/Solver.php
@@ -16,20 +16,48 @@ class Solver extends DayPuzzle
 
 	protected function part1()
 	{
+		$this->part1Logic($this->getFileAsArray('input'));
 	}
 
 	protected function part2()
 	{
+		$this->part2Logic($this->getFileAsArray('input'));
 	}
 
 	protected function part1Logic($input)
 	{
+		$overlaps = 0;
+		foreach ($input as $pair) {
+			[$f, $s] = explode(',', $pair);
+			$f = explode('-', $f);
+			$s = explode('-', $s);
 
+			if (
+				$f[0] >= $s[0] && $f[1] <= $s[1]
+				|| $f[0] <= $s[0] && $f[1] >= $s[1]
+			) {
+				$overlaps++;
+			}
+		}
+
+		printf("There were %d overlaps\n", $overlaps);
 	}
 
 	protected function part2Logic($input)
 	{
+		$overlaps = 0;
+		foreach ($input as $pair) {
+			[$f, $s] = explode(',', $pair);
+			$f = range(...explode('-', $f));
+			$s = range(...explode('-', $s));
 
+			if (count(array_intersect($f, $s))>0) {
+				$overlaps++;
+			}
+
+		}
+
+		printf("There were %d overlaps\n", $overlaps);
 	}
 
 	protected function getNamespace(): string
