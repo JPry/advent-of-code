23b4a4bff203f5ada9e3978e4697f2a832f5b753
Jeremy Pry <jeremy.pry@gmail.com>
2022-12-08 01:41:08 -0500

    Tweak the logic to determine how many visible per row

--- src/y2022/day08/Solver.php
+++ src/y2022/day08/Solver.php
@@ -31,108 +31,114 @@ class Solver extends DayPuzzle
 	protected function part1Logic(array $input)
 	{
 		$this->setMap(new Map($input));
-		$current = [0, 0];
 		$lastColumnIndex = $this->map->getLastColumnIndex();
 		$lastRowIndex = $this->map->getLastRowIndex();
-		$count = 0;
 
 		// Count the number of visible trees.
+		$current = [0, 0];
+		$counts = [];
+		$rowCount = 0;
 		do {
 			// Handle the first and last row with no extra logic
 			if ($current[0] === 0) {
-				$count += $this->map->getColumnCount();
+				$counts[0] = $this->map->getColumnCount();
 				$current[0]++;
 				continue;
 			}
 
 			if ($current[0] === $lastRowIndex) {
-				$count += $this->map->getColumnCount();
+				$counts[$lastRowIndex] = $this->map->getColumnCount();
 				break;
 			}
 
 			// Handle each column.
 			while ($current[1] <= $lastColumnIndex) {
+				$treeHeight = $this->map->getValue(new Point(...$current));
 				if ($current[1] === 0 || $current[1] === $lastColumnIndex) {
-					$count++;
+					$rowCount++;
+				} elseif ($treeHeight === 0) {
+					$current[1]++;
+					continue;
 				} else {
-					$treeHeight = $this->map->getValue(new Point(...$current));
-
 					// look west
 					$looking = $current;
 					do {
 						$looking[1]--;
 
-						// If a tree is taller, this one isn't visible, break out of this loop.
+						// If a tree is taller or the same height, this one isn't visible, break out of this loop.
 						if ($treeHeight <= $this->map->getValue(new Point(...$looking))) {
 							break;
 						}
 
 						// If we've gotten to the edge, the inner tree is taller than the rest.
 						if ($looking[1] === 0) {
-							$count++;
+							$rowCount++;
 							$current[1]++;
 							continue 2;
 						}
-					} while ($looking[1] > 0);
+					} while (true);
 
 					// look north
 					$looking = $current;
 					do {
 						$looking[0]--;
-						// If a tree is taller, this one isn't visible, break out of this loop.
+						// If a tree is taller or the same height, this one isn't visible, break out of this loop.
 						if ($treeHeight <= $this->map->getValue(new Point(...$looking))) {
 							break;
 						}
 
 						// If we've gotten to the edge, the inner tree is taller than the rest.
 						if ($looking[0] === 0) {
-							$count++;
+							$rowCount++;
 							$current[1]++;
 							continue 2;
 						}
-					} while ($looking[0] > 0);
+					} while (true);
 
 					// look east
 					$looking = $current;
 					do {
 						$looking[1]++;
-						// If a tree is taller, this one isn't visible, break out of this loop.
+						// If a tree is taller or the same height, this one isn't visible, break out of this loop.
 						if ($treeHeight <= $this->map->getValue(new Point(...$looking))) {
 							break;
 						}
 
 						// If we've gotten to the edge, the inner tree is taller than the rest.
 						if ($looking[1] === $lastColumnIndex) {
-							$count++;
+							$rowCount++;
 							$current[1]++;
 							continue 2;
 						}
-					} while ($looking[1] <= $lastColumnIndex);
+					} while (true);
 
 					// look south
 					$looking = $current;
 					do {
 						$looking[0]++;
-						// If a tree is taller, this one isn't visible, break out of this loop.
+						// If a tree is taller or the same height, this one isn't visible, break out of this loop.
 						if ($treeHeight < $this->map->getValue(new Point(...$looking))) {
 							break;
 						}
 
 						// If we've gotten to the edge, the inner tree is taller than the rest.
 						if ($looking[0] === $lastRowIndex) {
-							$count++;
+							$rowCount++;
 							$current[1]++;
 							continue 2;
 						}
-					} while ($looking[0] < $lastRowIndex);
+					} while (true);
 				}
 				$current[1]++;
 			}
+
+			$counts[$current[0]] = $rowCount;
 			$current[0]++;
 			$current[1] = 0;
+			$rowCount = 0;
 		} while (true);
 
-		printf("The number of visible trees is: %d\n", $count);
+		printf("The number of visible trees is: %d\n", array_sum($counts));
 	}
 
 	protected function part2Logic($input)
