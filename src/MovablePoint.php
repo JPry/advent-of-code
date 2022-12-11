fd4253bcaf6a4e70416c014f068b6fb3577756e6
Jeremy Pry <jeremy.pry@gmail.com>
2022-12-11 00:16:19 -0500

    Update attempt for day 9

---
+++ src/MovablePoint.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace JPry\AdventOfCode;
+
+use RuntimeException;
+
+/**
+ * @method void L(int $distance)
+ * @method void R(int $distance)
+ * @method void U(int $distance)
+ * @method void D(int $distance)
+ */
+class MovablePoint extends Point
+{
+
+	/**
+	 * is triggered when invoking inaccessible methods in an object context.
+	 *
+	 * @param string $name
+	 * @param array $arguments
+	 * @return void
+	 */
+	public function __call(string $name, array $arguments)
+	{
+		$distance = (int) $arguments[0];
+		switch ($name) {
+			case 'L':
+				$this->column -= $distance;
+				break;
+
+			case 'R':
+				$this->column += $distance;
+				break;
+
+			case 'U':
+				$this->row -= $distance;
+				break;
+
+			case 'D':
+				$this->row += $distance;
+				break;
+
+			default:
+				throw new RuntimeException('Invalid direction');
+		}
+	}
+
+	public function updateRow(int $newRow)
+	{
+		$this->row = $newRow;
+	}
+
+	public function updateColumn(int $newColumn)
+	{
+		$this->column = $newColumn;
+	}
+}
--- src/y2022/day09/Solver.php
+++ src/y2022/day09/Solver.php
@@ -5,6 +5,7 @@ namespace JPry\AdventOfCode\y2022\day09;
 
 use Exception;
 use JPry\AdventOfCode\DayPuzzle;
+use JPry\AdventOfCode\MovablePoint;
 use JPry\AdventOfCode\Point;
 use JPry\AdventOfCode\Utils\WalkResource;
 
@@ -13,13 +14,11 @@ class Solver extends DayPuzzle
 	use WalkResource;
 
 	/** @var array */
-	protected $map = [];
+	protected array $map = [];
 
-	/** @var int */
-	protected $row = 0;
+	protected ?MovablePoint $head;
 
-	/** @var int  */
-	protected $col = 0;
+	protected ?MovablePoint $tail;
 
 	public function runTests()
 	{
@@ -46,6 +45,7 @@ class Solver extends DayPuzzle
 
 	protected function part1()
 	{
+		$this->part1Logic($this->getHandleForFile('input'));
 	}
 
 	protected function part2()
@@ -59,32 +59,32 @@ class Solver extends DayPuzzle
 	protected function part1Logic($input)
 	{
 		// We need to track the head and tail of the rope.
-		$head = new Point(0,0);
-		$tail = new Point(0,0);
+		$this->head = new MovablePoint(4,0);
+		$this->tail = new MovablePoint(4,0);
 
 		// We need to track where the tail has been.
 
 		// Let's start with a 20x20 grid.
 		$this->map = array_fill(
 			0,
-			10,
-			array_fill(0, 10, 0)
+			5,
+			array_fill(0, 5, 0)
 		);
 
 		// We start at 0,0, so mark that as being visited
-		$this->map[0][0] = 1;
+		$this->map[4][0] = 1;
 
 		// We need to work through each line.
 		$this->walkResourceWithCallback(
 			$input,
-			function($line) use (&$head, &$tail) {
+			function($line) {
 				[$direction, $distance] = explode(' ', $line);
 				$distance = (int) $distance;
 
 				// See if we have enough room to move, and expand the map if necessary.
 				switch ($direction) {
 					case 'L':
-						$difference = $this->col - $distance;
+						$difference = $this->head->column - $distance;
 						if ($difference < 0) {
 							// Add more columns to the left
 							$this->addColumns('before', abs($difference));
@@ -92,22 +92,22 @@ class Solver extends DayPuzzle
 						break;
 
 					case 'R':
-						$difference = $this->col + $distance;
-						if ($difference >= count($this->map[0])) {
+						$difference = $this->head->column + $distance;
+						if ($difference > count($this->map[0])) {
 							$this->addColumns('after', $difference - count($this->map[0]));
 						}
 						break;
 
 					case 'U':
-						$difference = $this->row - $distance;
+						$difference = $this->head->row - $distance;
 						if ($difference < 0) {
 							$this->addRows('before', abs($difference));
 						}
 						break;
 
 					case 'D':
-						$difference = $this->row + $distance;
-						if ($difference >= count($this->map)) {
+						$difference = $this->head->row + $distance;
+						if ($difference > count($this->map)) {
 							$this->addRows('after', $difference - count($this->map));
 						}
 						break;
@@ -115,20 +115,60 @@ class Solver extends DayPuzzle
 
 				// Do the moving.
 				while ($distance > 0) {
+					// Move the head.
+					$this->head->{$direction}(1);
 
+					// Maybe move the tail
+					$this->maybeMoveTail();
 					$distance--;
 				}
 			}
 		);
+
+//		foreach ($this->map as $row) {
+//			foreach ($row as $column) {
+//				echo $column === 1 ? '#' : '.';
+//			}
+//			echo "\n";
+//		}
+
+		$sum = array_sum(
+			array_map(
+				function($array) {
+					return array_sum($array);
+				},
+				$this->map
+			)
+		);
+
+		printf("The tail visited %d locations\n", $sum);
 	}
 
-	protected function moveHeadAndTail(string $direction, string $distance)
+	protected function maybeMoveTail()
 	{
-		$distance = (int) $distance;
-		while ($distance > 0) {
+		$rowDifference = $this->head->row - $this->tail->row;
+		$columnDifference = $this->head->column - $this->tail->column;
 
-			$distance--;
+		$rowAbs = abs($rowDifference);
+		$columnAbs = abs($columnDifference);
+		$needsMoved = $rowAbs > 1 || $columnAbs > 1;
+		if (!$needsMoved) {
+			return;
 		}
+
+		// It needs moved, now figure out which way.
+		$rowMove = $this->tail->row + $rowDifference;
+		if ($rowAbs > 1 || $columnDifference === 0) {
+			$rowMove += $this->tail->row <=> $this->head->row;
+		}
+
+		$columnMove = $this->tail->column + $columnDifference;
+		if ($columnAbs > 1 || $rowDifference === 0) {
+			$columnMove += $this->tail->column <=> $this->head->column;
+		}
+		$this->tail->updateRow($rowMove);
+		$this->tail->updateColumn($columnMove);
+		$this->markVisited($this->tail);
 	}
 
 	protected function part2Logic($input)
@@ -138,7 +178,7 @@ class Solver extends DayPuzzle
 
 	protected function addRows(string $where, int $number)
 	{
-		printf("Adding new %d new rows %s\n", $number, $where);
+		printf("Adding %d new rows %s\n", $number, $where);
 		$cols = count($this->map[0]);
 		$newRows = array_fill(
 			0,
@@ -149,7 +189,8 @@ class Solver extends DayPuzzle
 		switch ($where) {
 			case 'before':
 				array_unshift($this->map, ...$newRows);
-				$this->row += $number;
+				$this->head->updateRow($this->head->row + $number);
+				$this->tail->updateRow($this->tail->row + $number);
 				break;
 
 			case 'after':
@@ -164,12 +205,13 @@ class Solver extends DayPuzzle
 	protected function addColumns(string $where, int $number)
 	{
 		$newColumns = array_fill(0, $number, 0);
-		printf("Adding new %d new columns %s\n", $number, $where);
+		printf("Adding %d new columns %s\n", $number, $where);
 		foreach ($this->map as &$row) {
 			switch ($where) {
 				case 'before':
 					$row = array_merge($newColumns, $row);
-					$this->col += $number;
+					$this->head->updateColumn($this->head->column + $number);
+					$this->tail->updateColumn($this->tail->column + $number);
 					break;
 
 				case 'after':
@@ -180,6 +222,15 @@ class Solver extends DayPuzzle
 					throw new Exception('Stop it, go to sleep');
 			}
 		}
+	}
+
+	/**
+	 * @param Point $point
+	 * @return void
+	 */
+	protected function markVisited(Point $point)
+	{
+		$this->map[$point->row][$point->column] = 1;
 	}
 
 	protected function getNamespace(): string
