c046a8d2ea192c579378f1d3f5d2e2f78c855190
Jeremy Pry <jeremy.pry@gmail.com>
2022-12-16 11:17:37 -0500

    Add template files for day 16

---
+++ src/y2022/day16/Solver.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace JPry\AdventOfCode\y2022\day16;
+
+use JPry\AdventOfCode\DayPuzzle;
+
+class Solver extends DayPuzzle
+{
+	public function runTests()
+	{
+		$data = $this->getFileAsArray('test');
+		$this->part1Logic($data);
+		$this->part2Logic($data);
+	}
+
+	protected function part1()
+	{
+	}
+
+	protected function part2()
+	{
+	}
+
+	protected function part1Logic($input)
+	{
+
+	}
+
+	protected function part2Logic($input)
+	{
+
+	}
+
+	protected function getNamespace(): string
+	{
+		return __NAMESPACE__;
+	}
+}
