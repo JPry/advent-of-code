3254c8c5004a94df82ae4b701d10ba2dbeb366ab
Jeremy Pry <jeremy.pry@gmail.com>
2022-12-21 12:11:51 -0500

    Add placeholders for remaining days

---
+++ src/y2022/day24/Solver.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace JPry\AdventOfCode\y2022\day24;
+
+use JPry\AdventOfCode\DayPuzzle;
+
+/**
+ * Day Solver Class.
+ *
+ * @link https://adventofcode.com/2022/day/24
+ */
+class Solver extends DayPuzzle
+{
+	public function runTests()
+	{
+		$data = $this->getFileAsArray('test');
+		$this->part1Logic($data);
+		$this->part2Logic($data);
+	}
+
+	protected function part1()
+	{
+	}
+
+	protected function part2()
+	{
+	}
+
+	protected function part1Logic($input)
+	{
+
+	}
+
+	protected function part2Logic($input)
+	{
+
+	}
+
+	protected function getNamespace(): string
+	{
+		return __NAMESPACE__;
+	}
+}
