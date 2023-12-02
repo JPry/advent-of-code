6c6c5ef46c28a91ebfac98da7ec15ea2b354049a
Jeremy Pry <jeremy.pry@gmail.com>
2023-12-02 11:36:47 -0600

    Add day 2 input and solution template

---
+++ src/y2023/day02/Solver.php
@@ -0,0 +1,44 @@
+<?php
+declare(strict_types=1);
+
+namespace JPry\AdventOfCode\y2023\day02;
+
+use JPry\AdventOfCode\DayPuzzle;
+
+/**
+ * Day Solver Class.
+ *
+ * @link https://adventofcode.com/2023/day/02
+ */
+class Solver extends DayPuzzle
+{
+	public function runTests()
+	{
+		$data = $this->getFileAsArray('test');
+		$this->part1Logic($data);
+		$this->part2Logic($data);
+	}
+
+	protected function part1()
+	{
+	}
+
+	protected function part2()
+	{
+	}
+
+	protected function part1Logic($input)
+	{
+
+	}
+
+	protected function part2Logic($input)
+	{
+
+	}
+
+	protected function getNamespace(): string
+	{
+		return __NAMESPACE__;
+	}
+}
