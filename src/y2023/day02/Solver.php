1e1ee6ccf7f4ecb8c01a587d3c804b465f6087b9
Jeremy Pry <jeremy.pry@gmail.com>
2023-12-02 22:28:07 -0600

    Add full solution for day 2

--- src/y2023/day02/Solver.php
+++ src/y2023/day02/Solver.php
@@ -21,10 +21,12 @@ class Solver extends DayPuzzle
 
 	protected function part1()
 	{
+		$this->part1Logic($this->getFileAsArray());
 	}
 
 	protected function part2()
 	{
+		$this->part2Logic($this->getFileAsArray());
 	}
 
 	protected function part1Logic($input)
@@ -32,21 +34,58 @@ class Solver extends DayPuzzle
 		$red = 12;
 		$green = 13;
 		$blue = 14;
-		
+
 		$valid = [];
-		
+
 		foreach ($input as $game) {
-			[$id, $details] = explode(':', $game);
+			[$id, $details] = explode(': ', $game);
 			$id = intval(preg_replace('#\D#', '', $id));
-			
-			$sets = explode(';', $details);
+
+			foreach (explode('; ', $details) as $set) {
+				foreach (explode(', ', $set) as $part) {
+					[$value, $color] = explode(' ', $part);
+					$color = trim($color);
+					$value = intval(trim($value));
+
+					if ($value > $$color) {
+						continue 3;
+					}
+				}
+			}
+
+			$valid[] = $id;
 		}
-		
+
+		$this->writeln('Valid ID sum: ' . array_sum($valid));
 	}
 
 	protected function part2Logic($input)
 	{
+		$sum = array_reduce(
+			$input,
+			function($carry, $line) {
+				$red = 1;
+				$blue = 1;
+				$green = 1;
 
+				[, $details] = explode(': ', $line);
+
+				foreach (explode('; ', $details) as $set) {
+					foreach (explode(', ', $set) as $part) {
+						[$value, $color] = explode(' ', $part);
+						$color = trim($color);
+						$value = intval(trim($value));
+
+						$$color = max($$color, $value);
+					}
+				}
+
+				return ($red * $blue * $green) + $carry;
+			},
+			0
+		);
+
+		$this->writeln('Part 2 power sum: ' . $sum);
 	}
 
 	protected function getNamespace(): string
