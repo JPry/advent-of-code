1791ded7b4f6c29da4b4eb5ae12937a144e61672
Jeremy Pry <jeremy.pry@gmail.com>
2024-12-01 21:27:01 -0600

    partial work on 2023 day 5

--- src/y2023/day05/Solver.php
+++ src/y2023/day05/Solver.php
@@ -14,7 +14,7 @@ class Solver extends DayPuzzle
 {
 	public function runTests()
 	{
-		$data = $this->getFileAsArray('test');
+		$data = $this->splitFileByDoubleNewLine('test');
 		$this->part1Logic($data);
 		$this->part2Logic($data);
 	}
@@ -27,9 +27,34 @@ class Solver extends DayPuzzle
 	{
 	}
 
-	protected function part1Logic($input)
+	/**
+	 * @param string[] $input
+	 * @return void
+	 */
+	protected function part1Logic(array $input)
 	{
+		$rawSeeds = array_shift($input);
+		[, $seeds] = explode(': ', $rawSeeds);
+		$seeds = array_map('intval', explode(' ', $seeds));
 
+		// Figure out min and max seeds so we don't need to look outside the needed range.
+		$minSeed = min($seeds);
+		$maxSeed = max($seeds);
+
+		$seed = array_combine(
+			range($minSeed, $maxSeed),
+			range($minSeed, $maxSeed),
+		);
+
+		$maps = ['seed' => []];
+		foreach ($input as $map) {
+			[$key, $values] = explode(" map:\n", $map);
+			[$source, $destination] = explode('-to-', $key);
+			$values = explode("\n", $values);
+			foreach ($values as $value) {
+				[$destinationStart, $sourceStart, $length] = explode(' ', $value);
+			}
+		}
 	}
 
 	protected function part2Logic($input)
