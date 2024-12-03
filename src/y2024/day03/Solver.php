c32dccde586e022c0289823d76e47a284e5632b2
Jeremy Pry <jeremy.pry@gmail.com>
2024-12-03 15:19:01 -0600

    Update test and solver for day 3

--- src/y2024/day03/Solver.php
+++ src/y2024/day03/Solver.php
@@ -14,26 +14,46 @@ class Solver extends DayPuzzle
 {
 	public function runTests()
 	{
-		$data = $this->getFileAsArray('test');
+		$data = $this->getFileContents('test');
 		$this->part1Logic($data);
 		$this->part2Logic($data);
 	}
 
 	protected function part1()
 	{
+		return $this->part1Logic($this->getFileContents());
 	}
 
 	protected function part2()
 	{
+		return $this->part2Logic($this->getFileContents());
 	}
 
 	protected function part1Logic($input)
 	{
+		$sums = [];
+		preg_match_all('#mul\((\d+),(\d+)\)#', $input, $matches);
+		foreach ($matches[0] as $index => $match) {
+			$sums[] = (int) $matches[1][$index] * (int) $matches[2][$index];
+		}
+
+		return array_sum($sums);
 	}
 
 	protected function part2Logic($input)
 	{
+		$sums = [];
+		$is_matching = true;
+		preg_match_all('#mul\((\d+),(\d+)\)|do\(\)|don\'t\(\)#', $input, $matches);
+		foreach ($matches[0] as $index => $match) {
+			match ($match) {
+				'do()' => $is_matching = true,
+				"don't()" => $is_matching = false,
+				default => $sums[] = $is_matching ? (int) $matches[1][$index] * (int) $matches[2][$index] : 0,
+			};
+		}
 
+		return array_sum($sums);
 	}
 
 	protected function getNamespace(): string
---
+++ tests/Year2024/Day03Test.php
@@ -0,0 +1,38 @@
+<?php
+
+use JPry\AdventOfCode\y2024\day03\Solver;
+
+describe('Part 1', function() {
+	it('should get the correct result for test input', function() {
+		$solver = new Solver();
+		expect($solver->returnTest1($solver->getFileContents('test')))->toBe(161);
+	});
+
+	it('should get the result for the input file', function() {
+		$solver = new Solver();
+		$result = $solver->returnPart1();
+		echo 'Result: ' . $result . PHP_EOL;
+		expect($result)->toBeGreaterThan(0);
+	});
+});
+
+describe('Part 2', function() {
+
+	it('should get the correct result for the test input', function() {
+		$solver = new Solver();
+		$input = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+		expect($solver->returnTest2($input))->toBe(48);
+	});
+
+	it('should get the correct result for the real input', function() {
+		$solver = new Solver();
+		$result = $solver->returnPart2();
+		printf(
+			'%s Result: %s%s',
+			basename(__FILE__),
+			$result,
+			PHP_EOL
+		);
+		expect($result)->toBeGreaterThan(0);
+	});
+});
