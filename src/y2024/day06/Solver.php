44ed97192f2386522c37c22cb66b58da25e568e7
Jeremy Pry <jeremy.pry@gmail.com>
2024-12-07 21:27:29 -0600

    Add initial files for days 4-7

---
+++ src/y2024/day06/Solver.php
@@ -0,0 +1,43 @@
+<?php
+declare(strict_types=1);
+
+namespace JPry\AdventOfCode\y2024\day06;
+
+use JPry\AdventOfCode\DayPuzzle;
+
+/**
+ * Day Solver Class.
+ *
+ * @link https://adventofcode.com/2024/day/06
+ */
+class Solver extends DayPuzzle
+{
+	public function runTests()
+	{
+		$data = $this->getFileAsArray('test');
+		$this->part1Logic($data);
+		$this->part2Logic($data);
+	}
+
+	protected function part1()
+	{
+	}
+
+	protected function part2()
+	{
+	}
+
+	protected function part1Logic($input)
+	{
+	}
+
+	protected function part2Logic($input)
+	{
+
+	}
+
+	protected function getNamespace(): string
+	{
+		return __NAMESPACE__;
+	}
+}
