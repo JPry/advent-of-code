89c0d58c91f222fd4e8c0227b6588ae4a4c918d0
Jeremy Pry <jeremy.pry@gmail.com>
2024-12-07 21:23:12 -0600

    Create some 2015 files

---
+++ tests/Year2015/Day02Test.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use JPry\AdventOfCode\y2015\day02\Solver;
+
+describe('Part 1', function() {
+	it('should get the correct result for test input', function() {
+		
+	})->todo(note: 'Need to write this test');
+
+	it('should get the result for the input file', function() {
+		
+	})->todo(note: 'Need to write this test');
+});
+
+describe('Part 2', function() {
+
+	it('should get the correct result for the test input', function() {
+		
+	})->todo(note: 'Need to write this test');
+
+	it('should get the correct result for the real input', function() {
+		
+	})->todo(note: 'Need to write this test');
+});
