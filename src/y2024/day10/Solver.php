b878e36d54e763e3deb033e14b090414360b0056
Jeremy Pry <jeremy.pry@gmail.com>
2024-12-10 23:02:02 -0600

    Add day 10 solutions

--- src/y2024/day10/Solver.php
+++ src/y2024/day10/Solver.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace JPry\AdventOfCode\y2024\day10;
 
 use JPry\AdventOfCode\DayPuzzle;
+use JPry\AdventOfCode\Map;
+use JPry\AdventOfCode\Point;
 
 /**
  * Day Solver Class.
@@ -12,6 +14,9 @@ use JPry\AdventOfCode\DayPuzzle;
  */
 class Solver extends DayPuzzle
 {
+	protected array $found = [];
+	protected bool $useFound = false;
+
 	public function runTests()
 	{
 		$data = $this->getFileAsArray('test');
@@ -21,23 +26,109 @@ class Solver extends DayPuzzle
 
 	protected function part1()
 	{
+		return $this->part1Logic($this->getFileAsArray());
 	}
 
 	protected function part2()
 	{
+		return $this->part2Logic($this->getFileAsArray());
 	}
 
 	protected function part1Logic($input)
 	{
+		$this->useFound = true;
+		$map = $this->setupMap($input);
+		$trailheads = $this->getTrailheads($map);
+
+		$value = 0;
+		foreach ($trailheads as $trailhead) {
+			$carry = 0;
+			$this->found = [];
+			$this->processPoint($trailhead, $map, $carry);
+			$value += $carry;
+		}
+
+		return $value;
+	}
+
+	protected function processPoint(Point $point, Map &$map, int &$carry): void
+	{
+		// Ensure we don't find the same point more than once.
+		if ($this->useFound && array_key_exists((string) $point, $this->found)) {
+			return;
+		}
+
+		$pointValue = $map->getValue($point);
+		if (9 === $pointValue) {
+			$carry++;
+			$this->found[(string) $point] = true;
+			return;
+		}
+
+		$nearbyPoints = $map->getOrthogonalPointsAround($point);
+		foreach ($nearbyPoints as $nearbyPoint) {
+			// If it's not the next higher number, then no need to process it.
+			if ($map->getValue($nearbyPoint) !== $pointValue + 1) {
+				continue;
+			}
+
+			$this->processPoint($nearbyPoint, $map, $carry);
+		}
 	}
 
 	protected function part2Logic($input)
 	{
+		$this->useFound = false;
+		$map = $this->setupMap($input);
+		$trailheads = $this->getTrailheads($map);
 
+		$value = 0;
+		foreach ($trailheads as $trailhead) {
+			$carry = 0;
+			$this->processPoint($trailhead, $map, $carry);
+			$value += $carry;
+		}
+
+		return $value;
 	}
 
 	protected function getNamespace(): string
 	{
 		return __NAMESPACE__;
+	}
+
+	/**
+	 * @param $input
+	 * @return Map
+	 */
+	protected function setupMap($input): Map
+	{
+		$mapped = [];
+		foreach ($input as $line) {
+			$mapped[] = array_map(
+				function ($item) {
+					return is_numeric($item) ? (int) $item : -1;
+				},
+				str_split($line)
+			);
+		}
+
+		$map = new Map($mapped);
+		return $map;
+	}
+
+	/**
+	 * @param Map $map
+	 * @return array
+	 */
+	protected function getTrailheads(Map $map): array
+	{
+		$trailheads = [];
+		$map->walkMap(function ($value, $row, $column) use (&$trailheads) {
+			if (0 === $value) {
+				$trailheads[] = new Point($row, $column);
+			}
+		});
+		return $trailheads;
 	}
 }
--- tests/Year2024/Day10Test.php
+++ tests/Year2024/Day10Test.php
@@ -5,22 +5,30 @@ declare(strict_types=1);
 use JPry\AdventOfCode\y2024\day10\Solver;
 
 describe('Part 1', function() {
-	it('should get the correct result for test input', function() {
-		
-	})->todo(note: 'Need to write this test');
+	it('should get the correct result for test input', function($data, $expected) {
+		$solver = new Solver();
+		$result = $solver->returnTest1(explode("\n", $data));
+		expect($result)->toBe($expected);
+	})->with('day 10');
 
 	it('should get the result for the input file', function() {
-		
-	})->todo(note: 'Need to write this test');
+		$result = (new Solver())->returnPart1();
+		printResult($result, __FILE__);
+		expect($result)->toBeGreaterThan(247)->toBe(694);
+	});
 });
 
 describe('Part 2', function() {
 
-	it('should get the correct result for the test input', function() {
-		
-	})->todo(note: 'Need to write this test');
+	it('should get the correct result for the test input', function($data, $expected) {
+		$solver = new Solver();
+		$result = $solver->returnTest2(explode("\n", $data));
+		expect($result)->toBe($expected);
+	})->with('day 10 part 2');
 
 	it('should get the correct result for the real input', function() {
-		
-	})->todo(note: 'Need to write this test');
+		$result = (new Solver())->returnPart2();
+		printResult($result, __FILE__);
+		expect($result)->toBeGreaterThan(0);
+	});
 });
